816dd53413cb57bc4b087c7c93ddae75cdb05894
Hippocrates <rimo52@gmail.com>
2022-12-03 02:13:11 +0300

    Семинар 03

---
+++ Seminar03/Task20/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 2D пространстве.
+
+Console.Clear();
+
+Console.WriteLine("Ведите координату точки A по X: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите координату точки A по Y: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите координату точки B по X: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите координату точки B по Y: ");
+int yb = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2));
+Console.WriteLine("Расстояние между точками: " + $"{distance:f3}");
---
+++ Seminar03/Task22/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+if (number == 0)
+{
+    Console.WriteLine("Ноль в любой степени будет 0");
+}
+// В случае если на вход пришло отрицательное число (-N)
+// то выводим таблицу квадратов чисел от -N до -1
+else if (number < 0)
+{
+    i = number;
+    number = -1;
+}
+while (i <= number)
+{
+    double square = Math.Pow(i, 2);
+    Console.Write($" {square} ");
+    i++;
+}
