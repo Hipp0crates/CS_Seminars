90c4e13a843d4aa1da9749b6fff790aae566f226
Hippocrates <rimo52@gmail.com>
2022-12-03 02:12:50 +0300

    Семинар 02 - Домашнее задание

---
+++ Seminar02HW/Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.WriteLine("Ведите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number < 1000)
+{
+    int secondnumber = (number % 100) / 10;
+    Console.WriteLine("Вторая цифра: " + secondnumber);
+}
+else
+{
+    Console.WriteLine("Введено не трехзначное число");
+}
---
+++ Seminar02HW/Task15/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели.
+// И проверяет, является ли этот день выходным.
+
+Console.Clear();
+
+Console.WriteLine("Введите номер дня недели от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 7 || number < 1)
+{
+    Console.WriteLine("Нет такого дня недели");
+}
+else
+{
+    if (number == 1) Console.Write("Понедельник");
+    if (number == 2) Console.Write("Вторник");
+    if (number == 3) Console.Write("Среда");
+    if (number == 4) Console.Write("Четверг");
+    if (number == 5) Console.Write("Пятница");
+    if (number == 6) Console.Write("Суббота");
+    if (number == 7) Console.Write("Воскресенье");
+    if (number > 5)
+    {
+        Console.Write(" - выходной день");
+    }
+    else
+    {
+        Console.Write(" - не является выходным днём");
+    }
+}
