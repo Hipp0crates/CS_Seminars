4dfc365bd80bdbed9a123d23cbe1dbaab3388efe
Hippocrates <rimo52@gmail.com>
2022-12-15 00:24:45 +0300

    Семинар 06 - Домашнее задание

---
+++ Seminar06HW/Task41/Program.cs
@@ -0,0 +1,44 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Ввод чисел останавливается при помощи ввода стоп-слова "stop"
+
+Console.Clear();
+
+Console.Write("Введите количество вводимых чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] enteredNumbers = new int[m];
+
+void InputNumbers(int m, string stopWord) // Ввод чисел пользователем
+{
+    Console.WriteLine($"Для остановки ввода наберите: {stopWord}");
+    int i = 0;
+    while (i < m)
+    {
+        Console.Write($"Введите число {i + 1}: ");
+        string? input = Console.ReadLine();
+        if (input == stopWord) break;
+        bool verification = int.TryParse(input, out int number);
+        if (verification == true)
+        {
+            enteredNumbers[i] = number;
+            i++;
+        }
+        else
+        {
+            Console.WriteLine("Введенные вами символы не являются числом");
+        }
+    }
+}
+
+int GreaterThanZero(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count += 1;
+    }
+    return count;
+}
+
+InputNumbers(m, "stop"); // Тут можно поменять стоп-слово
+Console.WriteLine($"Введено чисел больше 0: {GreaterThanZero(enteredNumbers)}");
---
+++ Seminar06HW/Task43/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2.
+// Значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Clear();
+
+double[,] values = new double[2, 2]; // Ввод данных в двумерный массив
+for (int i = 0; i < values.GetLength(0); i++)
+{
+    Console.WriteLine($"Введите коэффициенты {i + 1} уравнения (y = k{i + 1} * x + b{i + 1})");
+    for (int j = 0; j < values.GetLength(1); j++)
+    {
+        if (j == 0)
+        {
+            Console.Write($"Введите коэффициент k{i + 1}: ");
+        }
+        else
+        {
+            Console.Write($"Введите коэффициент b{i + 1}: ");
+        }
+        values[i, j] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+if (values[0, 0] == values[1, 0] && values[0, 1] == values[1, 1])
+{
+    Console.WriteLine("Прямые одинаковые");
+}
+else if (values[0, 0] == values[1, 0] && values[0, 1] != values[1, 1])
+{
+    Console.WriteLine($"Прямые параллельны");
+}
+else
+{
+    double pointX = (values[1, 1] - values[0, 1]) / (values[0, 0] - values[1, 0]);
+    double pointY = pointX * values[0, 0] + values[0, 1];
+    Console.WriteLine($"Точка пересечения прямых: ({pointX:f4}, {pointY:f4})"); // Округлил
+}
